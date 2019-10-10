f1f9291eb316f781306d3836272ab7eb06f1a2bd
windows10 <windows10@DESKTOP-TTPR51B>
2019-10-10 11:36:57 +0200

    2 modificació

--- Carrera/src/Moto.java
+++ Carrera/src/Moto.java
@@ -4,6 +4,7 @@ public class Moto {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// primera prova la programa feta despres de aixo de aixo tenim  que hacer cosas.
+		//nueva configuracion
 	}
 
 }
