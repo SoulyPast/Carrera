38a769054fe0339597bff35731e444472551a1c2
windows10 <windows10@DESKTOP-TTPR51B>
2019-10-10 11:32:30 +0200

    PRIMER comit

---
+++ Carrera/src/Moto.java
@@ -0,0 +1,9 @@
+
+public class Moto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// primera prova la programa feta despres de aixo de aixo tenim  que hacer cosas.
+	}
+
+}
